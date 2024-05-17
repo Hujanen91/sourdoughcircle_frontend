868df39815cf1be3976098f928f7d01ce453e475
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-17 12:38:46 +0000

    Edit logo on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
       <Navbar expand="md" fixed="top">
         <Container>
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="75" />
+            <img src={logo} alt="logo" height="90" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
