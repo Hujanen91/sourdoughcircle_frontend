52af831872a32a00b521c79c600b5c7d1fd2abd1
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-17 10:43:15 +0000

    Create navbar in NavBar.js and import icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/images/sourdoughWEBP.webp";
 
 const NavBar = () => {
   return (
-    <div>Navbar placeholder</div>
-  )
-}
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fa-duotone fa-house" style={{"--fa-primary-color": "#af4732", "--fa-secondary-color": "#ff7300",}}></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-duotone fa-right-to-bracket" style={{"--fa-primary-color": "#af4732", "--fa-secondary-color": "#ff7300",}}></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-duotone fa-user-plus" style={{"--fa-primary-color": "#af4732", "--fa-secondary-color": "#ff7300",}} ></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
