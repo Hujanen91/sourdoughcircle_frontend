88758e0c523feea9156d08a82c60b15b8f249042
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-17 12:23:57 +0000

    Edit icons on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,29 +3,29 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/images/sourdoughWEBP.webp";
 
 const NavBar = () => {
-  return (
-    <Navbar expand="md" fixed="top">
-      <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
-            <Nav.Link>
-              <i className="fa-duotone fa-house" style={{"--fa-primary-color": "#af4732", "--fa-secondary-color": "#ff7300",}}></i>Home
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa-duotone fa-right-to-bracket" style={{"--fa-primary-color": "#af4732", "--fa-secondary-color": "#ff7300",}}></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa-duotone fa-user-plus" style={{"--fa-primary-color": "#af4732", "--fa-secondary-color": "#ff7300",}} ></i>Sign up
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
-
-export default NavBar;
+    return (
+      <Navbar expand="md" fixed="top">
+        <Container>
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="75" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto text-left">
+              <Nav.Link>
+                <i className="fa fa-house-chimney-user" style={{color: "#af4732",}}/>Home
+              </Nav.Link>
+              <Nav.Link>
+                <i className="fa fa-arrow-right-to-bracket" style={{color: "#af4732",}}></i>Sign in
+              </Nav.Link>
+              <Nav.Link>
+                <i className="fa fa-user-plus" style={{color: "#af4732",}}></i>Sign up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  };
+  
+  export default NavBar;
