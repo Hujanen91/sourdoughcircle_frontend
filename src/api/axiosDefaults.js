5a27e38dffd846d217e5ee95b73e562f7af659c1
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-20 20:09:14 +0000

    Make access tokens refresh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://sourdoughcircle-api-382dc0f20c45.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
