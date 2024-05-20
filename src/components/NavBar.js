86cba3e1b3828590643deb472a99803000bbf1bb
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-20 10:49:03 +0000

    Install react router
    Implement the react functions in NavBar and App.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,48 @@
 import React from "react";
-import styles from '../styles/NavBar.module.css';
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/images/sourdoughWEBP.webp";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-      <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="90" />
+            <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-              <Nav.Link>
-                <i className="fa fa-house-chimney-user" style={{color: "#af4732",}}/>Home
-              </Nav.Link>
-              <Nav.Link>
-                <i className="fa fa-arrow-right-to-bracket" style={{color: "#af4732",}}></i>Sign in
-              </Nav.Link>
-              <Nav.Link>
-                <i className="fa fa-user-plus" style={{color: "#af4732",}}></i>Sign up
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
-  
-  export default NavBar;
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
