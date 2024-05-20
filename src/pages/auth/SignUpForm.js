237f34dcbe92e8d28c58e5e98d6b93f1583e712c
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-20 11:48:59 +0000

    Add signupform code

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -8,32 +8,95 @@ import appStyles from "../../App.module.css";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
-  return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
-
-            {/* add your form here */}
-
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={loginimg}
-        />
-      </Col>
-    </Row>
-  );
-};
-
-export default SignUpForm;
+    return (
+        <Row className={styles.Row}>
+          <Col className="my-auto py-2 p-md-2" md={6}>
+            <Container className={`${appStyles.Content} p-4 `}>
+              <h1 className={styles.Header}>sign up</h1>
+    
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="username">
+                  <Form.Label className="d-none">username</Form.Label>
+                  <Form.Control
+                    className={styles.Input}
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                {errors.username?.map((message, idx) => (
+                  <Alert variant="warning" key={idx}>
+                    {message}
+                  </Alert>
+                ))}
+    
+                <Form.Group controlId="password1">
+                  <Form.Label className="d-none">Password</Form.Label>
+                  <Form.Control
+                    className={styles.Input}
+                    type="password"
+                    placeholder="Password"
+                    name="password1"
+                    value={password1}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                {errors.password1?.map((message, idx) => (
+                  <Alert key={idx} variant="warning">
+                    {message}
+                  </Alert>
+                ))}
+    
+                <Form.Group controlId="password2">
+                  <Form.Label className="d-none">Confirm password</Form.Label>
+                  <Form.Control
+                    className={styles.Input}
+                    type="password"
+                    placeholder="Confirm password"
+                    name="password2"
+                    value={password2}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                {errors.password2?.map((message, idx) => (
+                  <Alert key={idx} variant="warning">
+                    {message}
+                  </Alert>
+                ))}
+    
+                <Button
+                  className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                  type="submit"
+                >
+                  Sign up
+                </Button>
+                {errors.non_field_errors?.map((message, idx) => (
+                  <Alert key={idx} variant="warning" className="mt-3">
+                    {message}
+                  </Alert>
+                ))}
+              </Form>
+            </Container>
+    
+            <Container className={`mt-3 ${appStyles.Content}`}>
+              <Link className={styles.Link} to="/signin">
+                Already have an account? <span>Sign in</span>
+              </Link>
+            </Container>
+          </Col>
+          <Col
+            md={6}
+            className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+          >
+            <Image
+              className={`${appStyles.FillerImage}`}
+              src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
+            />
+          </Col>
+        </Row>
+      );
+    };
+    
+    export default SignUpForm;
