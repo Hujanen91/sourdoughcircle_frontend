00b39685efe1b95b3893e4bbf1e1c390950ba374
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-20 10:57:34 +0000

    Edit colors on navlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/logo.png";
+import logo from "../assets/images/sourdoughWEBP.webp";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 
@@ -10,7 +10,7 @@ const NavBar = () => {
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="45" />
+            <img src={logo} alt="logo" height="75" />
           </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
