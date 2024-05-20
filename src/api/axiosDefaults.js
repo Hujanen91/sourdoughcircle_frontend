1551bfb22cd27d7e7958974bf64cbdccdfa39db7
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-20 11:34:32 +0000

    Install axios and wire it up

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://sourdoughcircle-api-382dc0f20c45.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
