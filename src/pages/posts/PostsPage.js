b831e9fbb658dbc3ccbfb054eb08efb411460c86
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-21 21:36:35 +0000

    Try to fix error with postcreation

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import Form from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form"
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -16,52 +16,52 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/images/search.png";
 
 function PostsPage({ message, filter = "" }) {
-    const [posts, setPosts] = useState({ results: [] });
-    const [hasLoaded, setHasLoaded] = useState(false);
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      const fetchPosts = async () => {
-        try {
-          const { data } = await axiosReq.get(`/posts/?${filter}`);
-          setPosts(data);
-          setHasLoaded(true);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-  
-      setHasLoaded(false);
-      fetchPosts();
-    }, [filter, pathname]);
-  
-    return (
-      <Row className="h-100">
-        <Col className="py-2 p-0 p-lg-2" lg={8}>
-          <p>Popular profiles mobile</p>
-          {hasLoaded ? (
-            <>
-              {posts.results.length ? (
-                posts.results.map((post) => (
-                  <Post key={post.id} {...post} setPosts={setPosts} />
-                ))
-              ) : (
-                <Container className={appStyles.Content}>
-                  <Asset src={NoResults} message={message} />
-                </Container>
-              )}
-            </>
-          ) : (
-            <Container className={appStyles.Content}>
-              <Asset spinner />
-            </Container>
-          )}
-        </Col>
-        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-          <p>Popular profiles for desktop</p>
-        </Col>
-      </Row>
-    );
-  }
-  
-  export default PostsPage;
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;
