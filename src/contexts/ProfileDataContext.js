02d0184e375dc3161d1852ec71cb37f3762d25f9
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-23 12:20:52 +0000

    Add unfollow code to utils & ProfileDataContext.js

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 import { followHelper } from "../utils/utils";
+import { followHelper, unfollowHelper } from "../utils/utils";
 
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
@@ -35,6 +36,28 @@ export const ProfileDataProvider = ({ children }) => {
           ...prevState.popularProfiles,
           results: prevState.popularProfiles.results.map((profile) =>
             followHelper(profile, clickedProfile, data.id)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnfollow = async (clickedProfile) => {
+    try {
+      await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
           ),
         },
       }));
