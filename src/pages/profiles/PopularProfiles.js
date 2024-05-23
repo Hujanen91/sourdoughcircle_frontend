8fb4b6260a753ca05fde23c78cc84fad9384b2d2
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-05-23 11:34:42 +0000

    Add profile images and follow button
    Where added in PopularProfiles.js
    
    to be displayed in most followed box

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,6 +4,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Profile from "./Profile";
 
 const PopularProfiles = ({ mobile }) => {
     const [profileData, setProfileData] = useState({
@@ -33,31 +34,31 @@ const PopularProfiles = ({ mobile }) => {
     }, [currentUser]);
   
     return (
-      <Container
-        className={`${appStyles.Content} ${
-          mobile && "d-lg-none text-center mb-3"
-        }`}
-      >
-        {popularProfiles.results.length ? (
-          <>
-            <p>Most followed profiles.</p>
-            {mobile ? (
-              <div className="d-flex justify-content-around">
-                {popularProfiles.results.slice(0, 4).map((profile) => (
-                  <p key={profile.id}>{profile.owner}</p>
-                ))}
-              </div>
-            ) : (
-              popularProfiles.results.map((profile) => (
-                <p key={profile.id}>{profile.owner}</p>
-              ))
-            )}
-          </>
-        ) : (
-          <Asset spinner />
-        )}
-      </Container>
-    );
-  };
+        <Container
+          className={`${appStyles.Content} ${
+            mobile && "d-lg-none text-center mb-3"
+          }`}
+        >
+          {popularProfiles.results.length ? (
+            <>
+              <p>Most followed profiles.</p>
+              {mobile ? (
+                <div className="d-flex justify-content-around">
+                  {popularProfiles.results.slice(0, 4).map((profile) => (
+                    <Profile key={profile.id} profile={profile} mobile />
+                  ))}
+                </div>
+              ) : (
+                popularProfiles.results.map((profile) => (
+                  <Profile key={profile.id} profile={profile} />
+                ))
+              )}
+            </>
+          ) : (
+            <Asset spinner />
+          )}
+        </Container>
+      );
+    };
   
   export default PopularProfiles;
