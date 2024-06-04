928f9796a3e62cd8d1f562ee66326d62e507f064
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-06-04 13:50:16 +0000

    Try to make categories work, not successful

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,20 +1,17 @@
 import React, { useState, useCallback } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+
 import appStyles from "../App.module.css";
 import Categories from "../constants/Categories";
 
-const CategoryFilter = ({ setFilter, mobile }) => {
-    const currentUser = useCurrentUser();
+const CategoryFilter = ({ onCategoryChange, mobile }) => {
     const [selectedCategory, setSelectedCategory] = useState('');
 
     const handleCategoryChange = useCallback((e) => {
         const selectedValue = e.target.value;
         setSelectedCategory(selectedValue);
-        setFilter(`category=${selectedValue}`);
-
-        if (!currentUser) return;
-    }, [setFilter, currentUser]);
+        onCategoryChange(selectedValue); // Call the prop function with the selected category
+  }, [onCategoryChange]);
 
     return (
         <Container className={`${appStyles.Content} mb-3 ${mobile ? "p-3 text-center container" : ""}`}>
